19688112cbfa19c37046303a1e7a17e03c324adc
djmisieq <zadroga.d@gmail.com>
2025-03-13 14:10:08 +0100

    Update App component to use ThemeProvider and AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import RequestForChange from './pages/RequestForChange';
 import ChangeDetails from './pages/ChangeDetails';
-import theme from './theme';
+import { ThemeProvider } from './theme/ThemeContext';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <ThemeProvider>
       <Router>
         <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/request" element={<RequestForChange />} />
-            <Route path="/change/:id" element={<ChangeDetails />} />
-          </Routes>
+          <AnimatePresence mode="wait">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/request" element={<RequestForChange />} />
+              <Route path="/change/:id" element={<ChangeDetails />} />
+            </Routes>
+          </AnimatePresence>
         </Layout>
       </Router>
     </ThemeProvider>
